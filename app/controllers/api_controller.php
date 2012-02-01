d981716b8aa46cddaa727f4574ff483203aa1457
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-02-01 20:02:57 +0100

    Integrated luniki's changes to allow trails with format, supersedes own implementation but preserves accept header recognition

--- app/controllers/api_controller.php
+++ app/controllers/api_controller.php
@@ -1,6 +1,94 @@
 <?
 class ApiController extends OAuthedController
 {
+# Adapted changes from https://github.com/luniki/trails/commit/6670fdefefd059d7a25a5b973e078f3e17385f04
+
+    /**
+     * This method extracts an action string and further arguments from it's
+     * parameter. The action string is mapped to a method being called afterwards
+     * using the said arguments. That method is called and a response object is
+     * generated, populated and sent back to the dispatcher.
+     *
+     * @param type <description>
+     *
+     * @return type <description>
+     */
+    function perform($unconsumed) {
+
+        list($action, $args, $format) = $this->extract_action_and_args($unconsumed);
+        # try to detect format
+        if (NULL === $format) {
+            foreach (words('json php xml csv') as $f) {
+                if (FALSE !== strpos($_SERVER['HTTP_ACCEPT'], $f)) {
+                    $format = $f;
+                    break;
+                }
+            }
+        }
+
+        # set format
+        $this->format = $format ?: 'html';
+
+        # call before filter
+        $before_filter_result = $this->before_filter($action, $args);
+
+        # send action to controller
+        # TODO (mlunzena) shouldn't the after filter be triggered too?
+        if (!(FALSE === $before_filter_result || $this->performed)) {
+
+            $mapped_action = $this->map_action($action);
+
+            # is action callable?
+            if (method_exists($this, $mapped_action)) {
+                call_user_func_array(array(&$this, $mapped_action), $args);
+            }
+            else {
+                $this->does_not_understand($action, $args);
+            }
+
+            if (!$this->performed) {
+                $this->render_action($action);
+            }
+
+            # call after filter
+            $this->after_filter($action, $args);
+        }
+
+        return $this->response;
+    }
+
+    /**
+    * Extracts action and args from a string.
+    *
+    * @param string the processed string
+    *
+    * @return array an array with two elements - a string containing the
+    * action and an array of strings representing the args
+    */
+    function extract_action_and_args($string) {
+
+        if ('' === $string) {
+            return array('index', array(), NULL);
+        }
+
+        $matched = preg_match('/^(?P<path>\w+(?:\/\w+)*)(?:\.(?P<format>\w+))?$/',
+                          $string, $matches);
+
+        if (!$matched) {
+            throw new Trails_Exception(400, "Bad Request");
+        }
+
+        $args = explode('/', $matches['path']);
+        $action = array_shift($args);
+        return array($action, $args, @$matches['format']);
+    }
+
+    function respond_to($ext) {
+        return $this->format === $ext;
+    }
+
+######
+
     function before_filter(&$action, &$args) {
         parent::before_filter($action, $args);
 
@@ -8,19 +96,6 @@ class ApiController extends OAuthedController
             $action = $GLOBALS['user']->id;
         }
 
-        list($action, $output_type) = explode('.', $action);
-        if (!$output_type) {
-            if (strpos($_SERVER['HTTP_ACCEPT'], 'csv') !== false) {
-                $output_type = 'csv';
-            } elseif (strpos($_SERVER['HTTP_ACCEPT'], 'php') !== false) {
-                $output_type = 'php';
-            } elseif (strpos($_SERVER['HTTP_ACCEPT'], 'xml') !== false) {
-                $output_type = 'xml';
-            } else {
-                $output_type = 'json';
-            }
-        }
-        
         $method = $_SERVER['REQUEST_METHOD'];
 
         if (!is_callable(array($this, $action . '_' . $method . '_action'))) {
@@ -29,12 +104,8 @@ class ApiController extends OAuthedController
         }
 
         $action = $action . '_' . $method;
-
-        $this->output_type = $output_type;
-        $this->action      = $action;
-        $this->args        = $args;
     }
-    
+
     function rescue($exception) {
         if (get_class($exception) !== 'Flexi_TemplateNotFoundException') {
             return parent::rescue($exception);
@@ -43,8 +114,8 @@ class ApiController extends OAuthedController
         $headers = array();
         $body    = '';
         $status  = 200;
-        
-        switch ($this->output_type) {
+
+        switch ($this->format) {
             case 'csv':
                 $headers['Content-Type'] = 'text/csv;charset=windows-1252';
                 $body .= self::csvEncode(array_keys(reset($this->data))) . "\n";
@@ -78,7 +149,7 @@ class ApiController extends OAuthedController
         }
         return $quote . str_replace($quote, $quote . $quote, $item) . $quote;
     }
-    
+
     private static function asXML($data, $parameters = array(), $pretty_print = true)
     {
         $dom = null;
@@ -92,11 +163,11 @@ class ApiController extends OAuthedController
             preg_match_all('/<(\w+)(?:\s[^>\/]*)?><\/\\1>/', $xml, $matches);
             $empty_tags = array();
             foreach ($matches as $index => $match) {
-                $index = '<EMPTY_TAG_' . md5($index) . '/>'; 
+                $index = '<EMPTY_TAG_' . md5($index) . '/>';
                 $empty_tags[$index] = $match[0];
             }
             $xml = str_replace(array_values($empty_tags), array_keys($empty_tags), $xml);
-            
+
             $level = 1;
             $indent = 0;
             $pretty = array();
@@ -124,7 +195,7 @@ class ApiController extends OAuthedController
                 }
             }
             $xml = implode("\n", $pretty);
-            
+
             $xml = str_replace(array_keys($empty_tags), array_values($empty_tags), $xml);
         }
         return $xml;
