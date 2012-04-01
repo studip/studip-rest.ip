98ffb639de555b882e203923b20abe011f025e4a
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-04-01 15:11:07 +0200

    add route for global system information

---
+++ routes/studip.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 
+ **/
+class StudipRoute implements APIPlugin
+{
+    /**
+     *
+     **/
+    function describeRoutes()
+    {
+        return array(
+            '/studip/settings' => _('Grundlegende Systemeinstellungen'),
+        );
+    }
+    
+    function routes(&$router)
+    {
+        $router->get('/studip/settings', function () use (&$router) {
+            $router->value(array(
+                'ALLOW_CHANGE_USERNAME' => $GLOBALS['ALLOW_CHANGE_USERNAME'],
+                'ALLOW_CHANGE_EMAIL'    => $GLOBALS['ALLOW_CHANGE_EMAIL'],
+                'ALLOW_CHANGE_NAME'     => $GLOBALS['ALLOW_CHANGE_NAME'],
+                'ALLOW_CHANGE_TITLE'    => $GLOBALS['ALLOW_CHANGE_TITLE'],
+                'INST_TYPE'             => $GLOBALS['INST_TYPE'],
+                'SEM_TYPE'              => $GLOBALS['SEM_TYPE'],
+                'SEM_CLASS'             => $GLOBALS['SEM_CLASS'],
+                'TERMIN_TYP'            => $GLOBALS['TERMIN_TYP'],
+                'PERS_TERMIN_KAT'       => $GLOBALS['PERS_TERMIN_KAT'],
+                'TITLES'                => $GLOBALS['DEFAULT_TITLE_FOR_STATUS'],
+                'UNI_NAME_CLEAN'        => $GLOBALS['UNI_NAME_CLEAN'],
+            ));
+        });
+    }
+}
