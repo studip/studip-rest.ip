401f9d9d355bb76d7aa4e7607e90665d5eb36267
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-04-05 23:39:13 +0200

    adjusted news route

--- routes/news.php
+++ routes/news.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace RestIP;
+use APIPlugin, StudipNews;
 
 /**
  *
  **/
-class NewsRoute implements \APIPlugin
+class NewsRoute implements APIPlugin
 {
     /**
      *
@@ -22,21 +23,30 @@ class NewsRoute implements \APIPlugin
      **/
     public function routes(&$router)
     {
-        $router->get('/news(/:range_id)', function ($range_id = false) use ($router)
+        $router->get('/news(/:range_id)', function ($range_id = false) use (&$router)
         {
             $range_id = $range_id ?: $GLOBALS['user']->id;
 
             require_once 'lib/classes/StudipNews.class.php';
 
-            $data = \StudipNews::GetNewsByRange($range_id);
+            $news = StudipNews::GetNewsByRange($range_id);
 
-            // Adjust data
-            array_walk($data, function (&$item) {
+            // Adjust data and extract users
+            $users = array();
+            array_walk($news, function (&$item) use (&$router, &$users) {
                 $item['body']       = formatReady($item['body']);
                 $item['chdate_uid'] = trim($item['chdate_uid']);
+                
+                if (!isset($users[$item['user_id']])) {
+                    $users[$item['user_id']] = $router->dispatch('get', '/user(/:user_id)', $item['user_id']);
+                }
+                if (!isset($users[$item['chdate_uid']])) {
+                    $users[$item['chdate_uid']] = $router->dispatch('get', '/user(/:user_id)', $item['chdate_uid']);
+                }
+                unset($item['author']);
             });
 
-            $router->value(array('news' => array_values($data)));
-        });
+            $router->value(compact('news', 'users'));
+        })->conditions(array('range_id' => '(studip|[a-f0-9]{32})'));
     }
 }
