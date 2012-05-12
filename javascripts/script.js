f29452d33a74e46761023c7a613271e13e35099a
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-05-12 17:34:01 +0200

    more documentation, json samples, syntax highlighting

---
+++ javascripts/script.js
@@ -0,0 +1,22 @@
+jQuery(function ($) {
+    $.litelighter.styles.mystyle = {
+        code: 'background-color:#141414;color:#ffffff;',
+        comment: 'color:#999;',
+        string: 'color:#8F9657;',
+        number: 'color:#CF6745;',
+        keyword: 'color:#6F87A8;'
+    };
+
+    $('pre[type="application/json"]').each(function () {
+        var content = $(this).text(), match;
+        if (match = $.trim(content).match(/^\{% include (.*?) %\}$/)) {
+            $(this).load('_includes/' + match[1], function () {
+                $(this).litelighter({
+                    clone: false,
+                    style: 'mystyle',
+                    language: 'js'
+                });
+            });
+        }
+    });
+});
