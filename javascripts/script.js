3c147f0f202aa62675b26549c0e3cd069b4e3cef
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-05-13 15:32:46 +0200

    remove js include workaround

--- javascripts/script.js
+++ javascripts/script.js
@@ -7,22 +7,9 @@ jQuery(function ($) {
         keyword: 'color:#6F87A8;'
     };
 
-    $('pre[type="application/json"]').each(function () {
-        var content = $(this).text(), match;
-        if (match = $.trim(content).match(/^\{% include (.*?) %\}$/)) {
-            $(this).load('_includes/' + match[1], function () {
-                $(this).litelighter({
-                    clone: false,
-                    style: 'mystyle',
-                    language: 'js'
-                });
-            });
-        } else {
-            $(this).litelighter({
-                clone: false,
-                style: 'mystyle',
-                language: 'js'
-            });
-        }
+    $('pre[type="application/json"]').litelighter({
+        clone: false,
+        style: 'mystyle',
+        language: 'js'
     });
 });
