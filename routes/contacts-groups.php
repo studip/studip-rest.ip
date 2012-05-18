a15890dc4fa530dc4e18db4eac94fc8a89d7c839
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-05-18 17:04:38 +0200

    new /contacts route, fixes #12

---
+++ routes/contacts-groups.php
@@ -0,0 +1,127 @@
+<?php
+namespace RestIP;
+use \DBManager, \PDO, \Request;
+
+class ContactsGroupsRoute implements \APIPlugin
+{
+    public function describeRoutes()
+    {
+        return array(
+            '/contacts' => _('Kontakte'),
+        );
+    }
+
+    public function routes(&$router)
+    {
+        require_once 'lib/statusgruppe.inc.php';
+
+        // Get all contact groups
+        $router->get('/contacts/groups', function () use ($router) {
+            $groups = ContactsGroups::load($GLOBALS['user']->id);
+            $router->render(compact('groups'));
+        });
+
+        // Create new contact group
+        $router->post('/contacts/groups', function () use ($router) {
+            $name = trim(Request::get('name'));
+            if (!$name) {
+                $router->halt(406, 'No name provided');
+            }
+
+            AddNewStatusgruppe($name, $GLOBALS['user']->id, 0);
+            $router->render($router->dispatch('get', '/contacts/groups'));
+        });
+
+        // Get members of contact group
+        $router->get('/contacts/groups/:group_id', function ($group_id) use ($router) {
+            if (!ContactsGroups::exists($group_id)) {
+                $router->halt(404, 'Contact group "%s" not found', $group_id);
+            }
+
+            $members = ContactsGroups::loadMembers($GLOBALS['user']->id, $group_id);
+
+            if ($router->compact()) {
+                $router->render(compact('members'));
+            }
+
+            $users = array();
+            foreach ($members as $user_id) {
+                $user = $router->dispatch('get', '/user(/:user_id)', $user_id);
+                $users[] = $user['user'];
+            }
+            $router->render(compact('members', 'users'));
+        });
+
+        // Remove contact group
+        $router->delete('/contacts/groups/:group_id', function ($group_id) use ($router) {
+            if (!ContactsGroups::exists($group_id)) {
+                $router->halt(404, 'Contact group "%s" not found', $group_id);
+            }
+            DeleteStatusgruppe($group_id);
+            $router->halt(200, 'Contact group "%s" has been deleted', $group_id);
+        });
+
+        // Put a user into contact group
+        $router->put('/contacts/groups/:group_id/:user_id', function ($group_id, $user_id) use ($router) {
+            if (!ContactsGroups::exists($group_id)) {
+                $router->halt(404, 'Contact group "%s" not found', $group_id);
+            }
+            $user = \User::find($user_id);
+            if (!$user) {
+                $router->halt(404, 'User "%s" not found', $user_id);
+            }
+            if (!InsertPersonStatusgruppe($user_id, $group_id)) {
+                $router->halt(500);
+            }
+            $router->render($router->dispatch('get', '/contacts/groups/:group_id', $group_id));
+        });
+
+        // Remove user from contact group
+        $router->delete('/contacts/groups/:group_id/:user_id', function ($group_id, $user_id) use ($router) {
+            if (!ContactsGroups::exists($group_id)) {
+                $router->halt(404, 'Contact group "%s" not found', $group_id);
+            }
+
+            $user = \User::find($user_id);
+            if (!$user) {
+                $router->halt(404, 'User "%s" not found', $user_id);
+            }
+            $username = $user->username;
+
+            RemovePersonStatusgruppe($username, $group_id);
+
+            $router->halt(200);
+        });
+    }
+}
+
+class ContactsGroups
+{
+    static function exists($group_id)
+    {
+        $query = "SELECT 1 FROM statusgruppen WHERE statusgruppe_id = ?";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($group_id));
+        return $statement->fetchColumn();
+    }
+
+    static function load($user_id)
+    {
+        $query = "SELECT statusgruppe_id AS group_id, name FROM statusgruppen WHERE range_id = ? ORDER BY position ASC";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($user_id));
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    static function loadMembers($user_id, $group_id)
+    {
+        $query = "SELECT user_id
+                  FROM statusgruppen
+                  JOIN statusgruppe_user USING (statusgruppe_id)
+                  WHERE range_id = ? AND statusgruppe_id = ?";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($user_id, $group_id));
+        return $statement->fetchAll(PDO::FETCH_COLUMN);
+    }
+
+}
---
+++ routes/contacts.php
@@ -0,0 +1,87 @@
+<?php
+namespace RestIP;
+use \DBManager, \PDO, \Request;
+
+class ContactsRoute implements \APIPlugin
+{
+    public function describeRoutes()
+    {
+        return array(
+            '/contacts' => _('Kontakte'),
+        );
+    }
+
+    public function routes(&$router)
+    {
+        require_once 'lib/contact.inc.php';
+
+        // Get all contact
+        $router->get('/contacts', function () use ($router) {
+            $contacts = Contacts::load($GLOBALS['user']->id);
+
+            if ($router->compact()) {
+                $router->render(compact('contacts'));
+                return;
+            }
+
+            $users = array();
+            foreach ($contacts as $user_id) {
+                $user = $router->dispatch('get', '/user(/:user_id)', $user_id);
+                $users[] = $user['user'];
+            }
+            $router->render(compact('contacts', 'users'));
+        });
+
+        // Add contact
+        $router->put('/contacts/:user_id', function ($user_id) use ($router) {
+            $user = \User::find($user_id);
+            if (!$user) {
+                $router->halt(404, 'User "%s" not found', $user_id);
+            }
+
+            $contact_id = Contacts::locate($GLOBALS['user']->id, $user_id);
+            if ($contact_id) {
+                $router->halt(406, 'User "%s" is already a contact', $user_id);
+            }
+
+            AddNewContact($user_id);
+
+            $router->render($router->dispatch('get', '/contacts'));
+        });
+
+        // Remove contact
+        $router->delete('/contacts/:user_id', function ($user_id) use ($router) {
+            $user = \User::find($user_id);
+            if (!$user) {
+                $router->halt(404, 'User "%s" not found', $user_id);
+            }
+
+            $contact_id = Contacts::locate($GLOBALS['user']->id, $user_id);
+            if (!$contact_id) {
+                $router->halt(406, 'User "%s" is not a contact', $user_id);
+            }
+            DeleteContact($contact_id);
+
+            $router->halt(200, 'Contact "%s" has been removed', $user_id);
+        });
+    }
+}
+
+class Contacts
+{
+    static function locate($user_id, $contact_id)
+    {
+        $query = "SELECT contact_id FROM contact WHERE owner_id = ? AND user_id = ?";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($user_id, $contact_id));
+        return $statement->fetchColumn();
+    }
+
+    static function load($user_id)
+    {
+        $query = "SELECT user_id FROM contact WHERE owner_id = ?";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($user_id));
+        return $statement->fetchAll(PDO::FETCH_COLUMN);
+    }
+}
