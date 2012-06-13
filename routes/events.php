c66c65475f95d7cfa450dc73e3dc44bab79085bc
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-06-13 15:50:40 +0200

    testing events

--- routes/events.php
+++ routes/events.php
@@ -1,5 +1,6 @@
 <?
 namespace RestIP;
+use \CalendarExportFile, \CalendarWriterICalendar, \Calendar;
 
 class EventsRoute implements \APIPlugin
 {
@@ -10,15 +11,23 @@ class EventsRoute implements \APIPlugin
     
     public static function before()
     {
-        require_once $GLOBALS['RELATIVE_PATH_CALENDAR'] . '/lib/sync/CalendarExport.class.php';
+        require_once $GLOBALS['RELATIVE_PATH_CALENDAR'] . '/lib/sync/CalendarExportFile.class.php';
         require_once $GLOBALS['RELATIVE_PATH_CALENDAR'] . '/lib/sync/CalendarWriterICalendar.class.php';
     }
     
     public function routes(&$router)
     {
         $router->get('/courses/:course_id/events', function ($course_id) use ($router) {
-            $export = new \CalendarExport(new \CalendarWriterICalendar());
-            $export->exportFromDatabase($course_id, 0, 2114377200, 'ALL_EVENTS');
+            $_calendar = Calendar::getInstance(Calendar::RANGE_SEM, $couse_id);
+            
+            
+            $export = new CalendarExportFile(new CalendarWriterICalendar());
+            $export->exportFromDatabase($course_id, 0, \Calendar::CALENDAR_END, 'ALL_EVENTS', array($course_id));
+            $export->sendFile();
+            die;
+            
+            var_dump($export->getExport());
+            die;
             
             $router->halt(501);
         });
