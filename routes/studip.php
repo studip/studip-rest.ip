35967d66b85f1df155692d09fa0e3383a2062076
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-07-03 15:19:43 +0200

    fix settings route

--- routes/studip.php
+++ routes/studip.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace RestIP;
+# namespace RestIP;
 
 /**
  *
  **/
-class StudipRoute implements \APIPlugin
+class StudipRoute implements APIPlugin
 {
     /**
      *
@@ -23,21 +23,32 @@ class StudipRoute implements \APIPlugin
     public function routes(&$router)
     {
         $router->get('/studip/settings', function () use ($router) {
+            $sem_types = array_map(function ($item) {
+                return array(
+                    'name'  => $item['name'],
+                    'class' => $item['class'],
+                );
+            }, SemType::getTypes());
+//            $mapper = function ($item) { return $item['name']; };
+            
             $router->render(array(
                 'ALLOW_CHANGE_USERNAME' => $GLOBALS['ALLOW_CHANGE_USERNAME'],
                 'ALLOW_CHANGE_EMAIL'    => $GLOBALS['ALLOW_CHANGE_EMAIL'],
                 'ALLOW_CHANGE_NAME'     => $GLOBALS['ALLOW_CHANGE_NAME'],
                 'ALLOW_CHANGE_TITLE'    => $GLOBALS['ALLOW_CHANGE_TITLE'],
                 'INST_TYPE'             => $GLOBALS['INST_TYPE'],
-                'SEM_TYPE'              => $GLOBALS['SEM_TYPE'],
-                'SEM_CLASS'             => $GLOBALS['SEM_CLASS'],
+                'SEM_TYPE'              => $sem_types,
+                'SEM_CLASS'             => array_map(function ($item) {
+                                               $item = (array)$item;
+                                               return reset($item);
+                                           }, SemClass::getClasses()),
                 'TERMIN_TYP'            => $GLOBALS['TERMIN_TYP'],
                 'PERS_TERMIN_KAT'       => $GLOBALS['PERS_TERMIN_KAT'],
                 'TITLES'                => $GLOBALS['DEFAULT_TITLE_FOR_STATUS'],
                 'UNI_NAME_CLEAN'        => $GLOBALS['UNI_NAME_CLEAN'],
             ));
         });
-        
+
         $router->get('/studip/colors', function () use ($router) {
             $colors = array();
             foreach ($GLOBALS['THEME']['COLORS'] as $key => $color) {
