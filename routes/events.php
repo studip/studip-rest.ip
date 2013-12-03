07decf1e989549a409a4de98d41c59664165ea15
aklassen <andre.klassen@gmail.com>
2013-12-03 16:33:43 +0100

    refine Event Route, add a suitabele description for a session

--- routes/events.php
+++ routes/events.php
@@ -1,7 +1,7 @@
 <?
 namespace RestIP;
 use \CalendarExport, \CalendarWriterICalendar, \Calendar, \DbCalendarEventList,
-    \SingleCalendar, \SingleDate, \Seminar;
+    \SingleCalendar, \SingleDate, \Seminar, \Issue;
 
 class EventsRoute implements \APIPlugin
 {
@@ -20,6 +20,7 @@ class EventsRoute implements \APIPlugin
         require_once $GLOBALS['RELATIVE_PATH_CALENDAR'] . '/lib/sync/CalendarWriterICalendar.class.php';
         require_once 'lib/classes/Seminar.class.php';
         require_once 'lib/raumzeit/raumzeit_functions.inc.php';
+        require_once 'lib/raumzeit/Issue.class.php';
 
         $router->get('/events', function () use ($router) {
             $start = time();
@@ -70,7 +71,22 @@ class EventsRoute implements \APIPlugin
             $dates = getAllSortedSingleDates($seminar);
 
             $events = array();
+            
             foreach ($dates as $date) {
+
+                //TODO: Use more of the SingleDate-functionalities
+
+                $issues = $date->getIssueIDs();
+                $issue_titles = array();
+                $description = '';
+                if(is_array($issues)) {
+                    foreach($issues as $is) {
+                        $issue = new Issue(array('issue_id' => $is));
+                        $issue_titles[] = $issue->getTitle();
+                    }
+                }
+                
+                $description = implode(', ', $issue_titles);
                 $temp = getTemplateDataForSingleDate($date);
                 $events[] = array(
                     'event_id'    => $date->getSingleDateID(),
@@ -78,10 +94,11 @@ class EventsRoute implements \APIPlugin
                     'start'       => $date->getStartTime(),
                     'end'         => $date->getEndTime(),
                     'title'       => $temp['date'],
-                    'description' => '',
+                    'description' => $description,
                     'categories'  => $temp['art'] ?: '',
                     'room'        => html_entity_decode(strip_tags($temp['room'] ?: '')),
                 );
+                
             }
 
             header('Cache-Control: private');
