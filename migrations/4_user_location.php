68c4b2a0082970e9606ff1d158f48b65576e041a
André <andre@gundk.it>
2015-12-02 10:58:57 +0100

    add new migration to store user localization data

---
+++ migrations/4_user_location.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ *
+ **/
+class UserLocation extends DBMigration
+{
+    /**
+     *
+     **/
+    public function description()
+    {
+        return _('Fügt eine neue Tabele für die Nutzerlokalisierung hinzu');
+    }
+
+    /**
+     *
+     **/
+    public function up ()
+    {
+        $query = "CREATE TABLE  IF NOT EXISTS `restip_user_location` (
+                    `user_id` varchar(32) NOT NULL,
+                    `ressource_id` varchar(32) NOT NULL,
+                    `geoLocation` text NOT NULL,
+                    `mkdate` int(20) NOT NULL,
+                    `chdate` int(20) NOT NULL,
+                    PRIMARY KEY (`user_id`))";
+         DBManager::get()->exec($query);
+
+    }
+
+    /**
+     *
+     **/
+    public function down()
+    {
+         // Delete user_location table
+        DBManager::get()->exec("DROP TABLE `restip_user_location`");
+    }
+}
